d5ea43f22291f66a06361dfddbb9c8c9f8412ec4
XuanyuZhao <Xuanyu.Zhao@gmail.com>
2016-03-04 13:28:32 +1100

    home page done

--- modules/articles/client/config/articles.client.config.js
+++ modules/articles/client/config/articles.client.config.js
@@ -9,23 +9,29 @@
 
   function menuConfig(Menus) {
     Menus.addMenuItem('topbar', {
-      title: 'Articles',
-      state: 'articles',
-      type: 'dropdown',
+      title: 'Find a tutor',
+      state: 'articles.list',
+
       roles: ['*']
+    });
+    Menus.addMenuItem('topbar', {
+      title: 'Register a tutor',
+      state: 'articles.create',
+
+      roles: ['user']
     });
 
     // Add the dropdown list item
-    Menus.addSubMenuItem('topbar', 'articles', {
-      title: 'List Articles',
-      state: 'articles.list'
-    });
-
-    // Add the dropdown create item
-    Menus.addSubMenuItem('topbar', 'articles', {
-      title: 'Create Article',
-      state: 'articles.create',
-      roles: ['user']
-    });
+    //Menus.addSubMenuItem('topbar', 'articles', {
+    //  title: 'List Articles',
+    //  state: 'articles.list'
+    //});
+    //
+    //// Add the dropdown create item
+    //Menus.addSubMenuItem('topbar', 'articles', {
+    //  title: 'Create Article',
+    //  state: 'articles.create',
+    //  roles: ['user']
+    //});
   }
 })();
